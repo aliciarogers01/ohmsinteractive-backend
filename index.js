81a8af02c5f00b390bb9e51416ee433183b62fa7
aliciarogers01 <126402805+aliciarogers01@users.noreply.github.com>
2026-04-17 19:55:59 -0400

    Change server listen address to '0.0.0.0'

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
   res.send("OHMS backend running");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT;
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
